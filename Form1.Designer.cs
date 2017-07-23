601dbeedb73a94b9afe34ce94dd38bc5efa5da91
lfschultz <lfschultz@ymail.com>
2017-07-23 12:10:34 -0500

    Added some basic form controls.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.appTitle = new System.Windows.Forms.Label();
+            this.btn_show_inventory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // appTitle
+            // 
+            this.appTitle.AutoSize = true;
+            this.appTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appTitle.Location = new System.Drawing.Point(240, 9);
+            this.appTitle.Name = "appTitle";
+            this.appTitle.Size = new System.Drawing.Size(224, 26);
+            this.appTitle.TabIndex = 0;
+            this.appTitle.Text = "Messy\'s Used Games";
+            // 
+            // btn_show_inventory
+            // 
+            this.btn_show_inventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_show_inventory.Location = new System.Drawing.Point(264, 67);
+            this.btn_show_inventory.Name = "btn_show_inventory";
+            this.btn_show_inventory.Size = new System.Drawing.Size(151, 34);
+            this.btn_show_inventory.TabIndex = 1;
+            this.btn_show_inventory.Text = "Current Inventory";
+            this.btn_show_inventory.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(740, 481);
+            this.Controls.Add(this.btn_show_inventory);
+            this.Controls.Add(this.appTitle);
+            this.Name = "Form1";
+            this.Text = "Inventory App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label appTitle;
+        private System.Windows.Forms.Button btn_show_inventory;
     }
 }
 
