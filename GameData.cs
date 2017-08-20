837d4f8943cfcb669b21683c748238fb22127d94
lfschultz <lfschultz@ymail.com>
2017-08-19 20:47:49 -0500

    Added messy-inventory namespace to GameData class.

--- GameData.cs
+++ GameData.cs
@@ -1,46 +1,49 @@
 ﻿using System;
 
-public class GameData
+namespace messy_inventory
 {
-    private string title;
-    private string console;
-    private string developer;
-    private string publisher;
-    private string condition;
-    private float price;
-
-	public GameData()
-	{
-        title = "";
-        console = "";
-        developer = "";
-        publisher = "";
-        condition = "";
-        price = 0.00F;
-    }
-
-    public GameData(string gameTitle, string gameConsole)
+    public class GameData
     {
-        title = gameTitle;
-        console = gameConsole;
-    }
+        private string title;
+        private string console;
+        private string developer;
+        private string publisher;
+        private string condition;
+        private float price;
 
-    public GameData(string gameTitle, string gameConsole, string gameCondition, float gamePrice)
-    {
-        title = gameTitle;
-        console = gameConsole;
-        condition = gameCondition;
-        price = gamePrice;
-    }
+        public GameData()
+        {
+            title = "";
+            console = "";
+            developer = "";
+            publisher = "";
+            condition = "";
+            price = 0.00F;
+        }
 
-    public GameData(string gameTitle, string gameConsole, string gameDev, string gamePublisher,
-        string gameCondition, float gamePrice)
-    {
-        title = gameTitle;
-        console = gameConsole;
-        developer = gameDev;
-        publisher = gamePublisher;
-        condition = gameCondition;
-        price = gamePrice;
+        public GameData(string gameTitle, string gameConsole)
+        {
+            title = gameTitle;
+            console = gameConsole;
+        }
+
+        public GameData(string gameTitle, string gameConsole, string gameCondition, float gamePrice)
+        {
+            title = gameTitle;
+            console = gameConsole;
+            condition = gameCondition;
+            price = gamePrice;
+        }
+
+        public GameData(string gameTitle, string gameConsole, string gameDev, string gamePublisher,
+            string gameCondition, float gamePrice)
+        {
+            title = gameTitle;
+            console = gameConsole;
+            developer = gameDev;
+            publisher = gamePublisher;
+            condition = gameCondition;
+            price = gamePrice;
+        }
     }
 }
