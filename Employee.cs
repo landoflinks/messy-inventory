fabc8634311ff713c23edf70bb25f7466a516390
lfschultz <lfschultz@ymail.com>
2017-08-19 20:37:18 -0500

    Added new Employee class.

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace messy_inventory
+{
+    class Employee
+    {
+    }
+}
