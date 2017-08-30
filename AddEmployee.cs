c75b5ce4ba0691f0cb0689ab8e081fb3ed42d208
lfschultz <lfschultz@ymail.com>
2017-08-29 19:26:56 -0500

    Added code to check for required fields.

--- AddEmployee.cs
+++ AddEmployee.cs
@@ -25,20 +25,40 @@ namespace messy_inventory
         private void CmbSubmit_Click(object sender, EventArgs e)
         {
             int employeeNum;
-            string firstName;
-            string lastName;
+            string first;
+            string last;
             string birthday;
             string position;
             string specialty;
             bool active;
 
             employeeNum = Convert.ToInt32(TxtEmployeeNum.Text);
-            firstName = TxtFirstName.Text;
-            lastName = TxtLastName.Text;
+            first = TxtFirstName.Text;
+            last = TxtLastName.Text;
             birthday = TxtBirthday.Text;
             position = CmbPosition.Text;
             specialty = TxtSpecialty.Text;
             active = ChkActive.Checked;
+
+            // Checking for required fields.
+            if (first == "")
+                {
+                    MessageBox.Show("You must enter a first name for this employee!", "Error", MessageBoxButtons.OK,
+                                       MessageBoxIcon.Stop);
+                    return;
+                }
+            if (last == "")
+            {
+                MessageBox.Show("You must enter a last name for this employee!", "Error", MessageBoxButtons.OK,
+                                   MessageBoxIcon.Stop);
+                return;
+            }
+            if (birthday == "" || birthday == "01/01/17")
+            {
+                MessageBox.Show("You must enter a date of birth for this employee!", "Error", MessageBoxButtons.OK,
+                                   MessageBoxIcon.Stop);
+                return;
+            }
         }
     }
 }
