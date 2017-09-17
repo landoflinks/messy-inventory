d0fb076b5e2afb37fd71b7de3a6d58bedb929993
lfschultz <lfschultz@ymail.com>
2017-09-16 19:04:25 -0500

    Some button rearranging.

--- MessyInventory.Designer.cs
+++ MessyInventory.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-            this.CmdShowInventory = new System.Windows.Forms.Button();
             this.CmdAddGame = new System.Windows.Forms.Button();
             this.CmdAddEmployee = new System.Windows.Forms.Button();
             this.CmdEmployeeList = new System.Windows.Forms.Button();
@@ -48,21 +47,10 @@
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Messy\'s Used Games";
             // 
-            // CmdShowInventory
-            // 
-            this.CmdShowInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CmdShowInventory.Location = new System.Drawing.Point(338, 67);
-            this.CmdShowInventory.Name = "CmdShowInventory";
-            this.CmdShowInventory.Size = new System.Drawing.Size(209, 34);
-            this.CmdShowInventory.TabIndex = 1;
-            this.CmdShowInventory.Text = "Show Current Inventory";
-            this.CmdShowInventory.UseVisualStyleBackColor = true;
-            this.CmdShowInventory.Click += new System.EventHandler(this.CmdShowInventory_Click);
-            // 
             // CmdAddGame
             // 
             this.CmdAddGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.CmdAddGame.Location = new System.Drawing.Point(12, 67);
+            this.CmdAddGame.Location = new System.Drawing.Point(12, 116);
             this.CmdAddGame.Name = "CmdAddGame";
             this.CmdAddGame.Size = new System.Drawing.Size(131, 34);
             this.CmdAddGame.TabIndex = 2;
@@ -73,7 +61,7 @@
             // CmdAddEmployee
             // 
             this.CmdAddEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.CmdAddEmployee.Location = new System.Drawing.Point(166, 67);
+            this.CmdAddEmployee.Location = new System.Drawing.Point(12, 67);
             this.CmdAddEmployee.Name = "CmdAddEmployee";
             this.CmdAddEmployee.Size = new System.Drawing.Size(131, 34);
             this.CmdAddEmployee.TabIndex = 3;
@@ -84,7 +72,7 @@
             // CmdEmployeeList
             // 
             this.CmdEmployeeList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CmdEmployeeList.Location = new System.Drawing.Point(568, 67);
+            this.CmdEmployeeList.Location = new System.Drawing.Point(300, 67);
             this.CmdEmployeeList.Name = "CmdEmployeeList";
             this.CmdEmployeeList.Size = new System.Drawing.Size(144, 34);
             this.CmdEmployeeList.TabIndex = 4;
@@ -96,7 +84,7 @@
             // 
             this.lblEditDescr.AutoSize = true;
             this.lblEditDescr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblEditDescr.Location = new System.Drawing.Point(15, 116);
+            this.lblEditDescr.Location = new System.Drawing.Point(15, 153);
             this.lblEditDescr.Name = "lblEditDescr";
             this.lblEditDescr.Size = new System.Drawing.Size(128, 17);
             this.lblEditDescr.TabIndex = 5;
@@ -105,9 +93,9 @@
             // DgvInventory
             // 
             this.DgvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvInventory.Location = new System.Drawing.Point(12, 136);
+            this.DgvInventory.Location = new System.Drawing.Point(12, 173);
             this.DgvInventory.Name = "DgvInventory";
-            this.DgvInventory.Size = new System.Drawing.Size(716, 333);
+            this.DgvInventory.Size = new System.Drawing.Size(716, 296);
             this.DgvInventory.TabIndex = 6;
             // 
             // FrmInventory
@@ -120,7 +108,6 @@
             this.Controls.Add(this.CmdEmployeeList);
             this.Controls.Add(this.CmdAddEmployee);
             this.Controls.Add(this.CmdAddGame);
-            this.Controls.Add(this.CmdShowInventory);
             this.Controls.Add(this.lblTitle);
             this.Name = "FrmInventory";
             this.Text = "Inventory Application";
@@ -133,7 +120,6 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Button CmdShowInventory;
         private System.Windows.Forms.Button CmdAddGame;
         private System.Windows.Forms.Button CmdAddEmployee;
         private System.Windows.Forms.Button CmdEmployeeList;
